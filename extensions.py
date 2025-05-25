880057ae367cc40d866c781983d68aa68df32079
Anuman <anuman181105@gmail.com>
2025-05-25 23:03:19 +0530

    extension changed

--- extensions.py
+++ extensions.py
@@ -1,10 +1,3 @@
-# extensions.py
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
 
-# Custom base class
-class Base(DeclarativeBase):
-    pass
-
-# Export the database object
-db = SQLAlchemy(model_class=Base)
+db = SQLAlchemy()
